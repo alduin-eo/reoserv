1be5259d6dad5a6ab3c269f98d0fe816d8dbee1b
Richard Leek <richard@richardleek.com>
2026-06-01 09:38:31 -0400

    Add database schema for transactions

---
+++ data/migrations/0004-alduin.mysql.sql
@@ -0,0 +1,25 @@
+CREATE TABLE
+    IF NOT EXISTS `character_transaction` (
+        `id` INTEGER NOT NULL AUTO_INCREMENT,
+        `character_id` INTEGER NOT NULL,
+        `action_id` INTEGER NOT NULL,
+        `amount` INTEGER NOT NULL,
+        `settled_amount` INTEGER NOT NULL DEFAULT 0,
+        `wallet_address` TEXT NOT NULL,
+        `status_id` INTEGER NOT NULL DEFAULT 0,
+        `created_at` INTEGER NOT NULL,
+        `resolved_at` INTEGER,
+        `resolved_by` INTEGER,
+        `notified` BOOLEAN NOT NULL DEFAULT 0,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`character_id`) REFERENCES `characters` (`id`)
+    );
+
+CREATE INDEX IF NOT EXISTS `idx_character_transaction_character_id_status`
+    ON `character_transaction` (`character_id`, `status_id`);
+
+CREATE INDEX IF NOT EXISTS `idx_character_transaction_status_notified`
+    ON `character_transaction` (`status_id`, `notified`);
+
+CREATE INDEX IF NOT EXISTS `idx_character_transaction_character_id_action_status`
+    ON `character_transaction` (`character_id`, `action_id`, `status_id`);
